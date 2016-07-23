d9a0e4e8c0afdcb19ed3bff4b14c0a7fb4fe9b77
Nathaniel Miller <dukeoflaser@gmail.com>
2016-07-23 17:45:03 -0600

    Clean up User model and update spec checklist.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,7 @@ class User < ActiveRecord::Base
      find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
        user.email = auth.info.email
        user.password = Devise.friendly_token[0,20]
-    #    user.name = auth.info.name   # assuming the user model has a name
-    #    user.image = auth.info.image # assuming the user model has an image
+       user.name = auth.info.name
      end
    end
 
