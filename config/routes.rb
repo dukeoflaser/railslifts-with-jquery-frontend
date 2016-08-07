ad921a90f411125898c6360fadb7f33d428a01c9
Nathaniel Miller <dukeoflaser@gmail.com>
2016-08-06 21:08:32 -0700

    Add Basic navigation links to profile page and program index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
     resources :programs
   end
 
-  resources :programs, only:[:index, :show]
+  resources :programs
 
   root to: "home#index"
 
-  get 'users/:id/next_workout', to: 'workouts#new', as: 'next_workout'
+  get '/next_workout', to: 'workouts#new', as: 'next_workout'
   get '/profile', to: 'users#show', as: 'profile'
   get '/my_programs', to: 'programs#index', as: 'my_programs'
 
