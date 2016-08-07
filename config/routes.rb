9e2906310a78134a21911a80bbf56dd2a0262570
Nathaniel Miller <dukeoflaser@gmail.com>
2016-08-06 20:47:47 -0700

    Change /users/:id/program route to /my_programs. Still uses the programs#index action.

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
   end
 
   resources :programs, only:[:index, :show]
+
   root to: "home#index"
 
   get 'users/:id/next_workout', to: 'workouts#new', as: 'next_workout'
   get '/profile', to: 'users#show', as: 'profile'
+  get '/my_programs', to: 'programs#index', as: 'my_programs'
 
 
 end
