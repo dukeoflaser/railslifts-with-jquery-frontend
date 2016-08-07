d980855144ef970f57ae52bf69747fc51ded7398
Nathaniel Miller <dukeoflaser@gmail.com>
2016-08-06 22:05:59 -0700

    Seperate /my_programs for /programs in programs#index. Adjust index view to work with updated index controller. Alternates links to /program and /my_programs depending on current URL

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,14 @@
 class ProgramsController < ApplicationController
   def index
+    if request.fullpath == "/my_programs"
+      @programs = current_user.programs
+      @current_programs_link = programs_path
+      @current_programs_text = 'View All Programs'
+    else
+      @programs = Program.all
+      @current_programs_link = my_programs_path
+      @current_programs_text = 'View My Programs'
+    end
   end
 
   def show
