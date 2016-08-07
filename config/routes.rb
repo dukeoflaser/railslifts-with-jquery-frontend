debf2868b5312223712c8b822ccb2fdf54a2e9b2
Nathaniel Miller <dukeoflaser@gmail.com>
2016-08-06 20:26:52 -0700

    Changed users/:id route to /profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  resources :users, only:[:show, :edit] do
+  resources :users, only:[:edit] do
     resources :programs
   end
 
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get 'users/:id/next_workout', to: 'workouts#new', as: 'next_workout'
+  get '/profile', to: 'users#show', as: 'profile'
 
 
 end
