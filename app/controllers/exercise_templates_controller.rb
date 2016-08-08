cb94b0e7a2e30b54ca957102b1f059db6a70f6ff
Nathaniel Miller <dukeoflaser@gmail.com>
2016-08-07 20:42:26 -0700

    Seperate exercise_templates from my_exercise_templates in exercise_templates#index.

--- app/controllers/exercise_templates_controller.rb
+++ app/controllers/exercise_templates_controller.rb
@@ -1,2 +1,15 @@
 class ExerciseTemplatesController < ApplicationController
+
+  def index
+    if request.fullpath == my_exercise_templates_path
+      @exercise_templates = current_user.exercise_templates
+      @current_exercise_templates_link = exercise_templates_path
+      @current_exercise_templates_text = 'View All Available Exercise Templates'
+    else
+      @exercise_templates = ExerciseTemplate.all
+      @current_exercise_templates_link = my_exercise_templates_path
+      @current_exercise_templates_text = 'View My Exercise Templates'
+    end
+  end
+
 end
