990c0e5c4882f9238ecfb6b2ac398c528f965a5d
Nathaniel Miller <dukeoflaser@gmail.com>
2016-08-16 14:02:03 -0700

    Add existing exercise button now prepopulates the exercise fields.

--- app/helpers/workout_templates_helper.rb
+++ app/helpers/workout_templates_helper.rb
@@ -8,4 +8,14 @@ module WorkoutTemplatesHelper
     end
   end
 
+  def id_to_attributes(v)
+    attributes = {}
+    template = ExerciseTemplate.find(v[:id])
+    attributes[:name] = template.name
+    attributes[:reps] = template.reps
+    attributes[:starting_weight] = template.starting_weight
+    attributes[:rest] = template.rest
+    attributes
+  end
+
 end
