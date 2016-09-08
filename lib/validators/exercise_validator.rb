c006adcd8f5ba66eefcb86e34b0b24fe56128548
Nathaniel Miller <dukeoflaser@gmail.com>
2016-09-08 15:34:34 -0700

    Fixed validation bug in lib/validators/exercise_validator.rb that was limiting the number of reps to a maximum of 9.

--- lib/validators/exercise_validator.rb
+++ lib/validators/exercise_validator.rb
@@ -2,15 +2,14 @@ module ExerciseValidator
 
   def reps_format
     unless correct_format?(reps)
-      errors.add(:reps, "must be a number from 1 to 20 and seperated by a space. Ex: 8 8 8")
+      errors.add(:reps, "must be between 1 and something reasonable as well as being seperated by a space. Ex: 8 8 8")
     end
   end
 
   def correct_format?(reps)
     reps.split(' ').each do |rep|
-      return false if /\A[1-9]+\z/.match(rep).nil?
-      return false unless (1...21) === rep.to_i
-      true
+      return false if /\A[\d]+\z/.match(rep).nil?
+      return (1...100) === rep.to_i
     end
   end
 
