0024f6e3746002ffceb808d61dcb5937e787bf61
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-26 22:49:48 -0700

    Add 'owner name' to program serializer.

--- app/serializers/program_serializer.rb
+++ app/serializers/program_serializer.rb
@@ -1,5 +1,4 @@
 class ProgramSerializer < ActiveModel::Serializer
-  attributes :id, :owner_id, :name, :description, :user_id
+  attributes :id, :owner_name, :name, :description, :user_id 
   has_many :workout_templates
-  # has_many :exercise_templates, through: :workout_templates
 end
