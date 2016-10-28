c38d0ef90c39efc599c3bf1750c4bed9ecf44d50
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-27 17:14:34 -0700

    Render programs index list of programs with jquery. Simplify programs_table partial and index view.

--- app/assets/javascripts/programs.js
+++ app/assets/javascripts/programs.js
@@ -56,3 +56,20 @@ function programsShow(){
 
   getProgramData();
 }
+
+function programsIndex(){
+
+  $.get('/programs.json', function(data){
+    $('.programList').html('');
+
+    if(data['programs'].length > 0){
+      data['programs'].forEach(function(program, i){
+        $('.programList').append('<tr class="program' + i + '"></tr>');
+        $('tr.program' + i).append('<td><a href="/programs/' + program['id'] + '">' + program['name'] + '</a></td>');
+        $('tr.program' + i).append('<td>' + program['workout_templates'].length + '</td>');
+      });
+    } else {
+      $('.programList').append('<tr><td>There are no programs to display.</td></tr>');
+    }
+  });
+}
