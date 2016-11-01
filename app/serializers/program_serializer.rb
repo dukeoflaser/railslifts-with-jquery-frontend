6eb0236564d31a0c0a4798dd5556e8bfe43f8369
Nathaniel Miller <dukeoflaser@gmail.com>
2016-11-01 15:41:48 -0700

    Add 'owner_id' to programs serialization.

--- app/serializers/program_serializer.rb
+++ app/serializers/program_serializer.rb
@@ -1,4 +1,4 @@
 class ProgramSerializer < ActiveModel::Serializer
-  attributes :id, :owner_name, :name, :description, :user_id 
+  attributes :id, :owner_name, :name, :description, :user_id, :owner_id
   has_many :workout_templates
 end
