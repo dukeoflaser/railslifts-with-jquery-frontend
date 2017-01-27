57760601182291d6e6412ffc28e210d774d48850
Nathaniel <dukeoflaser@gmail.com>
2017-01-27 14:40:43 -0800

    set initialize on precompile to false

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Railslifts
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.autoload_paths += %W(#{config.root}/lib/validators)
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
